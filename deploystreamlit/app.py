bfb89ad8c198a355ab3e6022d96fd632257668f1
Nadyaulia <112957809+Nadyaulia@users.noreply.github.com>
2025-06-14 09:33:47 +0700

    Update app.py

--- deploystreamlit/app.py
+++ deploystreamlit/app.py
@@ -5,11 +5,13 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler, LabelEncoder
+import os
 
-# Load the dataset
-@st.cache_data
+# Gunakan path relatif untuk memastikan file ditemukan
+DATA_PATH = "ObesityDataSet.csv"
+
 def load_data():
-    data = pd.read_csv("ObesityDataSet.csv")
+    data = pd.read_csv(DATA_PATH)
     return data
 
 # Preprocess the data
